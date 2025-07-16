b4c67c0e1eea0374418f5cff65783784a90cc2b0
Maxim <maxim@convey.de>
2025-07-16 12:21:35 +0200

    add deimos urls

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -10,7 +10,7 @@ const config = {
     // API Base URLs
     api: {
         baseURL: isProduction 
-            ? 'https://ls2fabackend-fthub6cvegdeagbv.westeurope-01.azurewebsites.net//api/v1' 
+            ? 'https://deimos.convey.de/2fabackend/api/v1' 
             : 'http://localhost:4000/api/v1',
         timeout: 30000 // 30 seconds
     },
@@ -20,7 +20,7 @@ const config = {
         name: 'LeadSuccess 2FA Portal',
         version: '1.0.0',
         frontendURL: isProduction 
-            ? 'https://brave-bay-061146803.1.azurestaticapps.net'
+            ? 'https://deimos.convey.de/ls2fa'
             : window.location.origin
     },
     
